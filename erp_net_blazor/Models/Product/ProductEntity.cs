c94f18b54ea081371e1f668595162c480db165e5
Evandro Cunha <evandrocunhajunior@gmail.com>
2025-10-26 01:14:23 -0300

    add default entities

--- erp_net_blazor/Models/Product/ProductEntity.cs
+++ erp_net_blazor/Models/Product/ProductEntity.cs
@@ -1,6 +1,12 @@
-﻿namespace erp_net_blazor.Models.Product
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace erp_net_blazor.Models.Product
 {
-    public class ProductEntity
+    public class ProductEntity : BaseEntity
     {
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [MaxLength(96)]
+        public string Name { get; set; } = string.Empty;
+
     }
 }
--- erp_net_blazor/Models/ProductFinancial/ProductFinancialEntity.cs
+++ erp_net_blazor/Models/ProductFinancial/ProductFinancialEntity.cs
@@ -1,6 +1,16 @@
-﻿namespace erp_net_blazor.Models.ProductFinancial
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace erp_net_blazor.Models.ProductFinancial
 {
-    public class ProductFinancialEntity
+    public class ProductFinancialEntity : BaseEntity
     {
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; } = 0;
+
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Cost { get; set; } = 0;
     }
 }
